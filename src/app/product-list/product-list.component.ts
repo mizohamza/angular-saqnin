071838cc7a2e756767e0aa8db42583e08fb16d52
mizohamza <hamza.smaiti@gmail.com>
2020-03-18 15:18:08 +0100

    3 files modified, 6 added

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-
 import { products } from '../products';
 
 @Component({
@@ -13,11 +12,7 @@ export class ProductListComponent {
   share() {
     window.alert('The product has been shared!');
   }
+  onNotify() {
+    window.alert('You will be notified when the product goes on sale');
+  }
 }
-
-
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at http://angular.io/license
-*/
